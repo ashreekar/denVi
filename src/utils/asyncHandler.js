88e1358026f7a9663b74d74336ab8cd5e23eb770
ashreekar <ashreekar767@gmail.com>
2025-08-09 19:10:03 +0530

    returned bug fixed in async handler

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,26 +1,26 @@
-const asyncHandler=(requestHandler)=>{
-    (req,res,next)=>{
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
         Promise.resolve(
-            requestHandler(req,res,next)
-        ).catch((err)=>{
+            requestHandler(req, res, next)
+        ).catch((err) => {
             next(err);
         })
     }
 }
 
-export {asyncHandler};
+export { asyncHandler };
 
 // other example
 
 // const asyncHandler=()=>{}
 // const asyncHandler=(fun)=>()=>{}
 // const asyncHandler=(fun)=>async ()=>{}
-// above is like 
+// above is like
 // const asyncHandler=(fun)=>{async ()=>{}}
 
-// const asyncHandler=(fun)=>async (req,res,next)=>{
+// const asyncHandler=(reqHandler)=>async (req,res,next)=>{
 //     try{
-//         await fun(req,res,next);
+//         await reqHandler(req,res,next);
 //     }catch(err){
 //         res.status(err.code || 500).json({
 //             sucess:false,
