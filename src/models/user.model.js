8bcf17aea293d8fe83c2061e3c8fe8603c4c6d9c
ashreekar <ashreekar767@gmail.com>
2025-08-19 13:39:08 +0530

    changed user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -27,11 +27,9 @@ const UserSchema = new Schema({
     avatar: {
         type: String,  // cloudinay
         required: true,
-        unique: true,
     },
     coverimage: {
         type: String,  // cloudinay
-        unique: true,
     },
     watchhistory: [
         {
@@ -52,7 +50,7 @@ const UserSchema = new Schema({
 
 UserSchema.pre("save", async function (next) {
     if (this.isModified("password")) {
-        this.password = bcrypt.hash(this.password, 10);
+        this.password = await bcrypt.hash(this.password, 10);
     }
 
     next();
