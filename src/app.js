225e8019d0675daa44147a7451401e39f58c851d
ashreekar <ashreekar767@gmail.com>
2025-08-22 19:18:20 +0530

    add user router controller

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser"; // to accease and crud cookines from user browser
 
-const app=express();
+const app = express();
 
 // now configure cookie and cors
 // app.use() for middlewares configuration
@@ -10,24 +10,28 @@ const app=express();
 app.use(cors(
     {
         origin: process.env.CORS_ORIGIN,
-        credentials:true
+        credentials: true
     }
 ));
-
 // limits the get file size by express by default
 app.use(express.json({
     limit: "16kb"
 }));
-
 // this helps to parse url like spaces and all which is endcided as %20
 app.use(express.urlencoded({
-    extended:true,
-    limit:"16kb"
+    extended: true,
+    limit: "16kb"
 }));
-
 // to store assest
 app.use(express.static('public'));
-
 app.use(cookieParser());
+
+// routes import
+
+import userRouter from './routes/user.routes.js';
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
+// https://localhost:8000/api/v1/user/register
 
 export { app };
